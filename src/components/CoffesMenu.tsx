4778df0c163e09bdf224682439a172f796568853
João Rambo <jpramboguanabara@hotmail.com>
2022-08-15 22:14:23 -0300

    a

--- src/components/CoffesMenu.tsx
+++ src/components/CoffesMenu.tsx
@@ -4,7 +4,7 @@ export default function CoffesMenu() {
     return (
         <div className="mt-24 mb-12">
             <h1 className='text-2xl font-title'>Nossos cafés</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-14 gap-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-14 gap-16">
                 <CoffeCard />
                 <CoffeCard />
                 <CoffeCard />
